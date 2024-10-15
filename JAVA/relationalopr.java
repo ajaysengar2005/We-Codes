e0087f855b3f03f41c92adbd7a1011101e4b9265
Vishva Pratap Singh Sengar <139144995+vishuubnf@users.noreply.github.com>
2024-10-15 22:29:31 +0530

    Create relationalopr.java

---
+++ JAVA/relationalopr.java
@@ -0,0 +1,17 @@
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 15;
+		
+		System.out.println("a == b : "+(a==b));
+		System.out.println("a != b : "+(a!=b));
+		System.out.println("a <= b : "+(a<=b));
+		System.out.println("a >= b : "+(a>=b));
+		System.out.println("a < b : "+(a<b));
+		System.out.println("a > b : "+(a>b));
+
+	}
+
+}
